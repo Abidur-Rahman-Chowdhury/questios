0622e7cf4bab9b21a40110b9125ed70ba9f68dbc
Abidur Rahman <abid43iiuc@gmail.com>
2021-07-12 00:09:06 +0600

    finished

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 //using selectors inside the element
+const question = document.querySelectorAll(".question");
+// console.log(question);
+question.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", function () {
+    question.classList.toggle("show-text");
+  });
+});
+
+// question.forEach(function (ques) {
+//   ques.classList.toggle("show-text");
+// });
+
 // traversing the dom
+// const questionBtn = document.querySelectorAll(".question-btn");
+
+// questionBtn.forEach(function (questionBtn) {
+//   questionBtn.addEventListener("click", function (e) {
+//     let question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
