2217613a4c96d2aec3afb87dd224744a29498bb1
Abidur Rahman <abid43iiuc@gmail.com>
2021-07-12 13:33:16 +0600

    fixed some bug

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 //using selectors inside the element
-const question = document.querySelectorAll(".question");
+const questions = document.querySelectorAll(".question");
 // console.log(question);
-question.forEach(function (question) {
+questions.forEach(function (question) {
   const btn = question.querySelector(".question-btn");
   btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
     question.classList.toggle("show-text");
   });
 });
 
-// question.forEach(function (ques) {
-//   ques.classList.toggle("show-text");
-// });
+question.forEach(function (ques) {
+  ques.classList.toggle("show-text");
+});
 
 // traversing the dom
 // const questionBtn = document.querySelectorAll(".question-btn");
